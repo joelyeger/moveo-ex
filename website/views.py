73545be5324c946dae108f1c5b5428589c282ada
[joel] <[joelyeger@gmail.com]>
2024-04-07 22:23:43 +0300

    message

--- website/views.py
+++ website/views.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from . import db
+from flask import Blueprint, render_template, redirect, url_for
 
 views = Blueprint('views', __name__)
 variables = {'cur': None, 'first': False}
@@ -9,7 +8,9 @@ variables = {'cur': None, 'first': False}
 def home():
     if variables["first"] and variables["cur"] is None:
         return redirect(url_for('views.wait'))
-
+    elif variables["cur"] is not None:
+        variables["first"] = False
+        return redirect(url_for(variables["cur"]))
     else:
         variables["first"] = True
         print("first connection is here, you must be the teacher")
@@ -20,38 +21,33 @@ def home():
 def hello():
 
     variables["cur"] = 'views.hello'
-    return render_template("hello.html")
+    return render_template("hello.html", is_read_only=variables["first"])
 
 
 @views.route('/counter', methods=['GET', 'POST'])
 def counter():
     variables["cur"] = 'views.counter'
 
-    return render_template("counter.html")
+    return render_template("counter.html", is_read_only=variables["first"])
 
 
 @views.route('/counter2', methods=['GET', 'POST'])
 def counter2():
     variables["cur"] = 'views.counter2'
 
-    return render_template("counter2.html")
+    return render_template("counter2.html", is_read_only=variables["first"])
 
 
 @views.route('/filter', methods=['GET', 'POST'])
 def filter():
     variables["cur"] = 'views.filter'
-
-    return render_template("filter.html")
+    return render_template("filter.html", is_read_only=variables["first"])
 
 
 @views.route('/wait', methods=['GET', 'POST'])
 def wait():
     if variables["cur"] is None:
         return render_template("wait.html")
+    variables["first"] = False
     return redirect(url_for(variables["cur"]))
 
-@views.route('/text', methods=['GET', 'POST'])
-def text():
-    variables["cur"] = 'views.text'
-
-    return render_template("text.html")
